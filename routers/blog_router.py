2fc469bf90c297952c675e0f03f65ed72a9415eb
marvinjoel <marvin.roses@gmail.com>
2023-01-24 20:54:04 -0500

    FEAT: get user by id

--- routers/blog_router.py
+++ routers/blog_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi_jwt_auth import AuthJWT
 from fastapi.encoders import jsonable_encoder
 
@@ -47,3 +47,16 @@ async def add_blog(blog: BlogModel, Authorize: AuthJWT = Depends()):
     }
 
     return jsonable_encoder(response)
+
+
+@blog_router.get('/user/{id}')
+async def get_blog_by_id(id: int, Authorize: AuthJWT = Depends()):
+    validate_authorize(Authorize)
+    subject = Authorize.get_jwt_subject()
+    current_user = session.query(User).filter(User.email == subject).first()
+    blogs = current_user.blog
+
+    for o in blogs:
+        if o.id == id:
+            return jsonable_encoder(o)
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not alowed to carry out request")
